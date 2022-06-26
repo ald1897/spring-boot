311cbef0063678a636f359908dea24978e4f3251
Alex Dickinson <alexdickinson04@gmail.com>
2022-06-25 21:56:08 -0400

    initial commit

--- src/main/java/hello/ChuckNorrisFact.java
+++ src/main/java/hello/ChuckNorrisFact.java
@@ -2,8 +2,19 @@ package hello;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CHUCK_NORRIS_FACT")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ChuckNorrisFact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
 
     private Long id;
 
--- src/main/java/hello/ChuckNorrisFactsRepository.java
+++ src/main/java/hello/ChuckNorrisFactsRepository.java
@@ -1,40 +1,50 @@
 package hello;
 
-import org.springframework.stereotype.Component;
+import org.springframework.data.repository.CrudRepository;
 
-import javax.swing.plaf.basic.BasicCheckBoxMenuItemUI;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+public interface ChuckNorrisFactsRepository extends CrudRepository<ChuckNorrisFact, Long> {
 
-@Component
-public class ChuckNorrisFactsRepository {
-
-    private final List<ChuckNorrisFact> facts = new ArrayList<>();
-
-    public ChuckNorrisFactsRepository() {
-        facts.add(createFact(1L, "Chuck Norris does not sleep, he waits"));
-        facts.add(createFact(2L, "Chuck Norris does not dial the wrong number, You answer the wrong phone."));
-    }
-
-    private ChuckNorrisFact createFact(final Long id, final String fact) {
-        final ChuckNorrisFact chuckNorrisFact = new ChuckNorrisFact();
-        chuckNorrisFact.setId(id);
-        chuckNorrisFact.setFact(fact);
-        return chuckNorrisFact;
-    }
-
-    public Iterable<ChuckNorrisFact> findAll() {
-        return Collections .unmodifiableList(facts);
-    }
-
-    public Optional<ChuckNorrisFact> findById(final Long id) {
-        for (final ChuckNorrisFact fact : facts) {
-            if (id.equals(fact.getId())) {
-                return Optional.of(fact);
-            }
-        }
-        return Optional.empty();
-    }
 }
+
+
+
+//package hello;
+//
+//import org.springframework.stereotype.Component;
+//
+//import javax.swing.plaf.basic.BasicCheckBoxMenuItemUI;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@Component
+//public class ChuckNorrisFactsRepository {
+//
+//    private final List<ChuckNorrisFact> facts = new ArrayList<>();
+//
+//    public ChuckNorrisFactsRepository() {
+//        facts.add(createFact(1L, "Chuck Norris does not sleep, he waits"));
+//        facts.add(createFact(2L, "Chuck Norris does not dial the wrong number, You answer the wrong phone."));
+//    }
+//
+//    private ChuckNorrisFact createFact(final Long id, final String fact) {
+//        final ChuckNorrisFact chuckNorrisFact = new ChuckNorrisFact();
+//        chuckNorrisFact.setId(id);
+//        chuckNorrisFact.setFact(fact);
+//        return chuckNorrisFact;
+//    }
+//
+//    public Iterable<ChuckNorrisFact> findAll() {
+//        return Collections .unmodifiableList(facts);
+//    }
+//
+//    public Optional<ChuckNorrisFact> findById(final Long id) {
+//        for (final ChuckNorrisFact fact : facts) {
+//            if (id.equals(fact.getId())) {
+//                return Optional.of(fact);
+//            }
+//        }
+//        return Optional.empty();
+//    }
+//}
